9263a218ba32ea4a1c336baedbf82fa1b683ac0e
Cécile Briard <cecile@artips.fr>
2017-04-18 17:28:12 +0200

    Donne le nom et le nombre de vélos disponibles à une station précise

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var request = require("request")
-var file = 'data.json'
 var jsonfile = require('jsonfile')
 
 var url = "https://api.jcdecaux.com/vls/v1/stations?contract=Paris&apiKey=2f5933819efdecf5d3773177bd309e9f9cff7463"
@@ -9,9 +8,12 @@ request({
     json: true
 }, function (error, response, body) {
 	if (!error && response.statusCode === 200) {
-		jsonfile.writeFile(file, body, function (err) {
-			console.error(err)
-		})
+		//jsonfile.writeFile(file, body, function (err) {
+		//	console.error(err)
+		//})
+
+		console.log("Station:", body[12].name)
+		console.log("Vélos disponibles:", body[12].available_bikes)
 		//var velosRestants = (body)
 		//var  = (body["available_bikes"])
 		}
