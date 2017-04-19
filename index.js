ed3c0c8b2b29fa5b68a23789510b37be585d1acd
Cécile Briard <cecile@artips.fr>
2017-04-19 16:23:17 +0200

    Bot cherche seulement les vélos restants pour une station indiquée
    Bot envoie un sms via Twilio avec les données récoltées

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 var request = require("request")
 var jsonfile = require('jsonfile')
+require('dotenv').config()
 
-var url = "https://api.jcdecaux.com/vls/v1/stations?contract=Paris&apiKey=2f5933819efdecf5d3773177bd309e9f9cff7463"
+var url = "https://api.jcdecaux.com/vls/v1/stations?contract=Paris&apiKey=2f5933819efdecf5d3773177bd309e9f9cff7463";
+var stationSearched = '20043';
 
 request({
     url: url,
     json: true
 }, function (error, response, body) {
+	var client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN); 
 	if (!error && response.statusCode === 200) {
-		//jsonfile.writeFile(file, body, function (err) {
-		//	console.error(err)
-		//})
-
-		console.log("Station:", body[12].name)
-		console.log("Vélos disponibles:", body[12].available_bikes)
-		//var velosRestants = (body)
-		//var  = (body["available_bikes"])
+		for (var i = 0; i < body.length; i++){
+			if (body[i].number == stationSearched && body[i].available_bikes < 5){
+				console.log("Station:", body[i].name)
+				console.log("Vélos disponibles:", body[i].available_bikes)
+				client.messages.create({ 
+					to: "+33782171637",
+					from: "+33644643476", 
+					body: "Il reste actuellement "+ body[i].available_bikes +" vélos à la station "+ body[i].name+"", 
+					}, function(err, message) {
+						if(err){
+							console.log(err);
+						} else {
+							console.log(message.body);
+					}
+				});
+			}
 		}
-	});
+	}
+	else {
+		console.error(error)
+	}
+});
